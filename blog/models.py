f78f93490a8a2ee711c9a2c28db9f37d10b875a8
Vladimir <angara99@gmail.com>
2019-06-03 15:20:34 +0300

    Blog added

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def cut(self):
+        split = self.body.split()
+        desc = ' '.join(split[:40])
+        return desc
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
